eec7a6b97b963203b728b9eebe9012d9902c9ccf
Maks-Chornyi <maksym.chornyi@oa.edu.ua>
2019-03-18 21:01:01 +0200

    added validation for required fields and human error message if errors exist

--- src/main/java/nl/mycubes/board/web/ProjectTaskController.java
+++ src/main/java/nl/mycubes/board/web/ProjectTaskController.java
@@ -5,7 +5,13 @@ import nl.mycubes.board.service.ProjectTaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequestMapping("/api/board")
 @RestController
@@ -16,9 +22,20 @@ public class ProjectTaskController {
     ProjectTaskService projectTaskService;
 
     @PostMapping("")
-    public ResponseEntity<?> addPTToBoard(@RequestBody ProjectTask projectTask) {
+    public ResponseEntity<?> addPTToBoard(@Valid @RequestBody ProjectTask projectTask, BindingResult result) {
+
+        if (result.hasErrors()) {
+            Map<String, String> errorMap = new HashMap<>();
+            for (FieldError error : result.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+
+            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+        } else {
             ProjectTask newProjectTask = projectTaskService.saveOrUpdateProjectTask(projectTask);
-            return new ResponseEntity<ProjectTask>(newProjectTask, HttpStatus.CREATED);
+            return new ResponseEntity<ProjectTask>(newProjectTask, HttpStatus.CREATED); 
+        }
+
     }
 
 }
